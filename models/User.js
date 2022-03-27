171e2032047ae709d63fc4f9c72260e3448605a1
VENKATA SAIKIRAN KATTEKOLA <vn857734@dal.ca>
2022-03-27 13:11:37 -0300

    UserMgmt: Added new fields to the model

--- models/User.js
+++ models/User.js
@@ -3,11 +3,13 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
+    _id: {type: String},
     user_id: { type: String, required: true, trim: true },
     password: { type: String, required: true },
     email: { type: String, required: true, trim: true },
     ngo_name: { type: String, required: true, trim: true },
     target_group: { type: String, required: true, trim: true },
+    status: { type: String, required: true, trim: true },
   },
   { timestamps: true }
 );
