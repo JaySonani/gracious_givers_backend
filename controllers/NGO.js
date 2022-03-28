4811d6489e67173d70583b49532d8815b6b5bb30
Jay Nimeshkumar Patel <jy982893@dal.ca>
2022-03-28 03:34:11 -0300

    added NGO update status logic

--- controllers/NGO.js
+++ controllers/NGO.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const AdminUser = require("../models/AdminUser");
 
+const serverErrorResponse = {
+    message: "Internal error occured at the server",
+    success: false,
+}
+
 exports.getPendingNGOs = async (request, response, next) => {
     let condition = {
         status: "Pending Admin Approval",
@@ -80,3 +85,59 @@ exports.getNgo = async (request, response, next) => {
             response.status(500).send(errorResponse);
         });
 };
+
+exports.updateStatus = async (request, response, next) => {
+    const ngoId = request.params.id;
+    const newStatusValue = request.params.status;
+    const invalidStatusChange = {
+        message: "Cannot update the NGO with status " + newStatusValue,
+        success: false,
+    }
+
+    if ("Active" === newStatusValue || 
+        "Deactivated" === newStatusValue) {
+        User.findById(ngoId)
+        .then(ngo => {
+            console.log(ngo);
+            if (!ngo) {
+                const errorResponse = {
+                    message: "Ngo with id " + ngoId + " not found",
+                    success: false,
+                }
+                response.status(404).send(errorResponse);
+            }
+            else {
+                const updatedNgo = new User({
+                    status: newStatusValue,
+                })
+                console.log(User);
+                User.findByIdAndUpdate(
+                    { _id: ngoId }, {status: newStatusValue}, (error, ngo) => {
+                        if (error) {
+                            console.log(error);
+                            response.status(500).send(serverErrorResponse);
+                        } 
+                        else 
+                        {                            
+                            const successResponse = {
+                                message: "Ngo status updated successfully",
+                                success: true
+                            }
+                            response.status(200).send(successResponse);                            
+                        }
+                    }
+                )   
+            };
+        })
+        .catch(error => {
+            console.log(error);
+            console.log("Error while retrieving ngo with ID :" + ngoId);
+            response.status(500).send(serverErrorResponse);
+        });  
+    }
+    else 
+    {
+        response.status(400).send(invalidStatusChange);
+    }
+    
+}
