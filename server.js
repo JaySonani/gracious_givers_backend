3986ea192255f8d96f2b9fbf096a5833c7e2ec84
Arjun Naravula Loganathan <ar302948@dal.ca>
2022-03-29 17:48:12 -0300

    Added notification to main

--- server.js
+++ server.js
@@ -17,11 +17,8 @@ app.use(cors())
 const donationRoute = require("./routes/donation");
 const fundraiserRoute = require("./routes/fundraiser");
 const authorizationRoute = require("./routes/authorizationRoute");
-<<<<<<< HEAD
 const notificationRoute = require("./routes/notification");
-=======
 const ngoRoute = require("./routes/ngo");
->>>>>>> origin
 
 // Default URL of backend
 app.get("/", (request, response) => {
@@ -31,12 +28,9 @@ app.get("/", (request, response) => {
 // Define all routes here
 app.use("/donation", donationRoute);
 app.use("/fundraiser", fundraiserRoute);
-<<<<<<< HEAD
 app.use("/getNotification", notificationRoute);
-=======
 app.use("/auth", authorizationRoute);
 app.use("/ngo", ngoRoute);
->>>>>>> origin
 
 const { static } = require("express");
 app.use("/images/", static("./uploads/fundraiser/image"));
