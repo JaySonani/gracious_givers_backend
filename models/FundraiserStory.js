b427bf507811118c7834c30bc86f76f5016ecfdd
viraj <viraj.shah@dal.ca>
2022-04-01 14:23:57 -0300

    database schema added for funsraiser story added

---
+++ models/FundraiserStory.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const uploadSchema = new mongoose.Schema({
+    // filename : {
+    //     type : String,
+    //     unique : true,
+    //     required: true
+    // },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+        //required: true
+    }
+})
+
+module.exports = mongoose.model('uploadImage', uploadSchema);
