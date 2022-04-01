68c8ece06baecef720d904a1b6d96b81df502145
viraj <viraj.shah@dal.ca>
2022-04-01 16:49:58 -0300

    Static files path added for middleware in server.js

--- server.js
+++ server.js
@@ -34,6 +34,17 @@ app.use("/auth", authorizationRoute);
 app.use("/ngo", ngoRoute);
 app.use("/photoGallery", photoGallery)
 
+// serving static files
+const path = require("path")
+app.use("/public", express.static(path.join(__dirname, "public")));
+//app.use("/server/uploads", express.static(path.join(__dirname, "server/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(
+    "uploads",
+    express.static(path.join(__dirname, "uploads"))
+);
+
+
 const { static } = require("express");
 app.use("/images/", static("./uploads/fundraiser/image"));
 
