812572cee12530801ce96ddcfe0c1cffe3a5c650
viraj <viraj.shah@dal.ca>
2022-04-01 16:28:24 -0300

    multer.js updated

--- middleware/multer.js
+++ middleware/multer.js
@@ -21,21 +21,3 @@ const imageFileFilter = (req, file, cb) => {
 const upload = multer({ storage: storage, fileFilter: imageFileFilter });
 
 module.exports = upload
-
-
-// const multer = require('multer');
-
-// // set storage
-// var storage = multer.diskStorage({
-//     destination : function ( req , file , cb ){
-//         cb(null, 'uploads')
-//     },
-//     filename : function (req, file , cb){
-//         // image.jpg
-//         var ext = file.originalname.substr(file.originalname.lastIndexOf('.'));
-
-//         cb(null, file.fieldname + '-' + Date.now() + ext)
-//     }
-// })
-
-// module.exports = store = multer({ storage : storage })
